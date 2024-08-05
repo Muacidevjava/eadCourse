1fba89206647dd25dfbcd0cb393fabd8b6f351b7
muaci <muaci@yahoo.com>
2024-08-05 16:39:53 -0300

    Adicionando FetchType Eager-Lazy, @EntityGraph, FetchMode

--- src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -2,8 +2,18 @@ package com.ead.course.repositories;
 
 import com.ead.course.models.ModuleModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
+
+
+    @Query(value = "SELECT * FROM tb_modules WHERE course_id = :courseId", nativeQuery = true)
+    List<ModuleModel> findAllLModulesIntoCourse(@Param("courseId") UUID courseId);
+
+
 }
