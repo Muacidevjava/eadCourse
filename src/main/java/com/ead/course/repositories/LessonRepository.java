ad17fd1d16563055d98bbb98d30d47722167fcc0
muaci <muaci@yahoo.com>
2024-08-06 13:37:10 -0300

    . Filtros  em APIs com Specification

--- src/main/java/com/ead/course/repositories/LessonRepository.java
+++ src/main/java/com/ead/course/repositories/LessonRepository.java
@@ -1,7 +1,11 @@
 package com.ead.course.repositories;
 
 import com.ead.course.models.LessonModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -9,10 +13,12 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface LessonRepository extends JpaRepository<LessonModel, UUID> {
+public interface LessonRepository extends JpaRepository<LessonModel, UUID>, JpaSpecificationExecutor<LessonModel> {
     @Query(value="select * from tb_lessons where module_module_id = :moduleId", nativeQuery = true)
     List<LessonModel> findAllLessonsIntoModule(@Param("moduleId") UUID moduleId);
 
     @Query(value = "SELECT * FROM tb_lessons WHERE module_module_id = :moduleId AND lesson_id = :lessonId", nativeQuery = true)
     Optional<LessonModel> findLessonIntoModule(@Param("moduleId") UUID moduleId, @Param("lessonId") UUID lessonId);
+
+    Page<LessonModel> findAll(Specification<LessonModel> spec, Pageable pageable);
 }
